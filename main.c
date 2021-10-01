a720e7fdecc70faa9c1fbed37ae55ac786c9c479
Sourov <76588896+sourovdeybd71@users.noreply.github.com>
2021-10-01 12:51:32 +0000

    Added Evaluation for a postfix expression

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include<string.h>
-
+#include<math.h>
 //char stack
 char stack[100];
 int top = -1;
@@ -96,6 +96,51 @@ void infixToPostfix(char infix[],char postfix[]) {
    postfix[k]='\0';
 
 }
+//Evaluation for POSTFIX expression
+int stack2[25];
+int top2 =-1;
+
+void push2(int item){
+  stack2[++top2] = item;
+
+}
+char pop2(){
+  return stack2[top2--];
+}
+
+float evaluate_postfix(char postfix[]){
+  char ch;
+  int i, operand1, operand2;
+
+  i=0;
+  while ((ch = postfix [i++])!= '\0'){
+    //if it is a digit then make it integer value via ch-0
+    if(isdigit(ch)){
+      push2(ch-'0');
+    }
+    //if it is not a digit then its a operand so do the next
+    else{
+      operand2=pop2();
+      operand1 = pop2();
+
+      switch(ch){
+        case '-':
+            push2(operand1-operand2);
+            break;
+        case '+':
+            push2(operand1+operand2);
+            break;
+        case '*':
+            push2(operand1*operand2);
+            break;
+        case '/':
+            push2(operand1/operand2);
+            break;
+      }
+    }
+  }
+  return stack2[top2];
+}
 
 int main(void) {
   char infix[100], postfix[100];
@@ -107,4 +152,8 @@ int main(void) {
   infixToPostfix(infix, postfix);
 
   printf("Postfix Expression: %s\n" , postfix);
+
+  int result= evaluate_postfix(postfix);
+  printf("Result: %d", result);
+
 }
